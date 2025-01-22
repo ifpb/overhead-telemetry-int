39797b0ff612dba8a76d8a577d929fe1e62b4de9
Rebeca Dantas <becadcabral@gmail.com>
2025-01-22 18:30:00 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-vagrant ssh host-2 -c "cd /vagrant/code && sudo timeout 60 ./receive.py" &
+vagrant ssh host-2 -c "cd /vagrant/code && sudo timeout 3600 ./receive.py" &
 
-vagrant ssh host-1 -c "cd /vagrant/code && sudo timeout 60 ./send.py 192.168.50.12" &
+vagrant ssh host-1 -c "cd /vagrant/code && sudo timeout 3600 ./send.py 192.168.50.12" &
 
 vagrant ssh host-2 -c "export DISPLAY=:0 && timeout 3600 vlc http://192.168.50.11/videos/worldcup2002-mp4.mpd" &
 
